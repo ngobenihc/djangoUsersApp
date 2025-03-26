4116398951eae372d87a04ce4f085dd2abc4e870
ngobenihc <ngobenihc@yahoo.com>
2025-03-26 11:31:32 +0200

    generate and create user password

--- users/serializer.py
+++ users/serializer.py
@@ -3,6 +3,15 @@ from rest_framework import serializers
 
 
 class UserSerializer(serializers.ModelSerializer):
+    username = serializers.CharField(read_only=True)
+    password =serializers.CharField(write_only=True,required=False)
+
+    def create(self, validated_data):
+        password = validated_data.pop("password")
+        user = User.objects.create(**validated_data)
+        user.set_password(password)
+        user.save()
+        return user
     class Meta:
         model = User
         fields=['url','id','username','email','first_name','last_name','password']
