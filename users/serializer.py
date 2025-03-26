b4ff6d66fdf473a810e5ba98cda82190ce1f17ef
ngobenihc <ngobenihc@yahoo.com>
2025-03-26 11:41:30 +0200

    change user password

--- users/serializer.py
+++ users/serializer.py
@@ -12,6 +12,23 @@ class UserSerializer(serializers.ModelSerializer):
         user.set_password(password)
         user.save()
         return user
+
+
+
+    def update(self, instance, validated_data):
+        try:
+            user = instance
+            password = validated_data.pop("password")
+            odl_password = validated_data.pop("odl_password")
+            if user.check_password(odl_password):
+                user.set_password(password)
+            else:
+                raise Exception("odl_password doesn't match")
+            user.save()
+        except Exception as err:
+            raise serializers.ValidationError(err)
+        return super(UserSerializer,self).update(instance, validated_data)
+
     class Meta:
         model = User
-        fields=['url','id','username','email','first_name','last_name','password']
+        fields=['url','id','username','email','first_name','last_name','password','odl_password']
